f255f8d78ffbd77786f1203edd0f0f9b4ba90b90
Jonathan Brouwers <yobrother_koffieboon@hotmail.com>
2017-12-25 12:31:56 +0100

    Move notes. Add game structure document. Add initial methods to main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
+def initialize_board():
+    pass
+
+
+def initialize_players(no_players):
+    pass
+
+
+def game_loop():
+    print("Hello World!")
+
+
 def main():
     won = False
     while not won:
         # make sure this is not an endless loop
+        game_loop()
         won = True
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
